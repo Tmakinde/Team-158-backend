69ef75c7965d42d18aa2197f35ba1d9cf889005b
Tmakinde <makindet74@gmail.com>
2020-09-16 09:54:28 +0100

    Done with adding farmer username to database

--- Team-158-farmnance-api/routes/api.php
+++ Team-158-farmnance-api/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//sign up route
+Route::post('/farmer/signup/', 'FarmerController@store'); //save username form facebook to database
+
